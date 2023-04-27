29206e6c71b02bfdeff81fe6d3a9f14e6a6a7825
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2023-04-27 00:18:13 -0400

    Remoção da linha 60 no arquivo att_cadastro

--- saveEdit.php
+++ saveEdit.php
@@ -20,17 +20,15 @@
     if(isset($_POST['atualizar'])){
       
         if (!empty($_POST['id']) && !empty($_POST['email']) && !empty($_POST['senha']) && !empty($_POST['telefone']) && !empty($_POST['github'])) {
+            
             $id = $_POST ['id'];
             $email = $_POST ['email'];
             $senha = $_POST ['senha'];
             $telefone = $_POST ['telefone'];
             $github = $_POST ['github'];
-            
 
-            // $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
-
-            // $result = $conn ->query($sqlUpdate);
         }
+        
         $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
         $result = $conn ->query($sqlUpdate);
     }
