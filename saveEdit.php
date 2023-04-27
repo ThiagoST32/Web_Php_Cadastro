ae85e21ac42f73e96aed645dd5dcb512a5d41ecb
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2023-04-27 00:09:11 -0400

    Mudanças e adilções da linha 23 a 35

--- saveEdit.php
+++ saveEdit.php
@@ -20,12 +20,20 @@
     if(isset($_POST['atualizar'])){
       
         if (!empty($_POST['id']) && !empty($_POST['email']) && !empty($_POST['senha']) && !empty($_POST['telefone']) && !empty($_POST['github'])) {
+            $id = $_POST ['id'];
+            $email = $_POST ['email'];
+            $senha = $_POST ['senha'];
+            $telefone = $_POST ['telefone'];
+            $github = $_POST ['github'];
             
-            $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
 
-            $result = $conn ->query($sqlUpdate);
+            // $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
+
+            // $result = $conn ->query($sqlUpdate);
         }
+        $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
+        $result = $conn ->query($sqlUpdate);
     }
 
-    header("Location: sistema.php");
+    // header("Location: sistema.php");
 ?>
