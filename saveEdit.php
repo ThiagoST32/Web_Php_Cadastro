6dbd26a4c831eb18b558fe3a8ff926184db29b55
ThiagoST32 <thiago.sandre.trevisan@gmail.com>
2023-05-10 13:25:10 -0400

    Remodelação de todo o arquivo

--- saveEdit.php
+++ saveEdit.php
@@ -1,38 +1,32 @@
 <?php
     include_once('connect.php');
-    include_once('att_cadastro.php');
-        
-    $id = $_GET['id'];
-        
-        $sql = "SELECT * FROM cadastro WHERE id=$id";
-        
-        $result = mysqli_query($conn, $sql);
-        
-        //trnasforma o resulta em array e armazena na var linha
-        $row = mysqli_fetch_assoc($result);
-        
-        //atribui na variavel nome o valor que vem da tabela
-        $id = $row ['id'];
-        $email = $row ['email'];
-        $senha = $row ['senha'];
-        $telefone = $row ['telefone'];
-        $github = $row ['github'];
 
+    // if(isset($_POST['atualizar']))
+    // {
+    //     $email = $_POST ['email'];
+    //     $senha = $_POST ['senha'];
+    //     $telefone = $_POST ['telefone'];         
+    //     $github = $_POST ['github'];
+        
+    //     $sqlInsert ="UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
+    //     $result = $conn->query($sqlInsert);
+    //     print_r($result);
+    //     echo $email;
+    // }
     if(isset($_POST['atualizar'])){
-      
-        if (!empty($_POST['id']) && !empty($_POST['email']) && !empty($_POST['senha']) && !empty($_POST['telefone']) && !empty($_POST['github'])) {
+        if (!empty($_POST['email']) && !empty($_POST['senha']) && !empty($_POST['telefone']) && !empty($_POST['github'])) {
             
-            $id = $user_data ['id'];
-            $email = $user_data ['email'];
-            $senha = $user_data ['senha'];
-            $telefone = $user_data ['telefone'];
-            $github = $user_data ['github'];
-
+            $id = $_POST ['id'];
+            $email = $_POST ['email'];
+            $senha = $_POST ['senha'];
+            $telefone = $_POST ['telefone'];
+            $github = $_POST ['github'];
         }
 
         $sqlUpdate = "UPDATE cadastro SET email='$email', senha='$senha', telefone='$telefone', github='$github' WHERE id='$id'";
-        $result = $conn ->query($sqlUpdate);
+        $result = $conn->query($sqlUpdate);
     }
+    
 
-    // header("Location: sistema.php");
+    header("Location: tabela.php");
 ?>
